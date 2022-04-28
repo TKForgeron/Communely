b3c7f64b42ab2f71971b44236930668d2f2b86ca
TKForgeron <trollololollolol.js@gmail.com>
2022-04-28 12:58:57 +0200

    add quotes service spring boot init (Java17, Jar, mvn)

---
+++ QuotesService/src/test/java/com/communely/quotes/quotes/QuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.communely.quotes.quotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
