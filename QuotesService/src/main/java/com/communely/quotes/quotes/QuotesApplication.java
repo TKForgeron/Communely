2d8452de38519c3ca463798e49334104d2cfc8da
TKForgeron <trollololollolol.js@gmail.com>
2022-05-10 21:48:08 +0200

    set up docker and mongodb

--- QuotesService/src/main/java/com/communely/quotes/quotes/QuotesApplication.java
+++ QuotesService/src/main/java/com/communely/quotes/quotes/QuotesApplication.java
@@ -2,12 +2,13 @@ package com.communely.quotes.quotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class QuotesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuotesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QuotesApplication.class, args);
+    }
 
 }
